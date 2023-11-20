23df0ed47a118624b2e78efa5a9c2bf71c1306ab
Emir TAS <emirtas3894@hotmail.com>
2023-11-20 23:36:36 +0100

    Ajout du dossier transformer qui contient les différents blocs de l'architecture d'un transformer classique sous forme de classes.

---
+++ transformer/MultiHeadAttention.py
@@ -0,0 +1,68 @@
+"""
+Project : CamemBERT
+Unit : Advanced Machine Learning 
+MSc. Intelligent systems engineering
+SORBONNE UNIVERSITÉ
+
+--- Students ---
+@KishanthanKingston - Kishanthan KINGSTON
+@SSivanesan - Shivamshan SIVANESAN
+@Emirtas7 - Emir TAS
+"""
+
+# MultiHeadAttention.py
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, num_heads) :
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads # nombre de têtes d'attention 
+        self.d_model = d_model # dimension des embeddings pour chaque token = dimension d'entrée 
+        
+        self.query = nn.Linear(d_model, d_model) 
+        self.key = nn.Linear(d_model, d_model)
+        self.value = nn.Linear(d_model, d_model)
+        
+        self.output = nn.Linear(d_model, d_model)
+    
+    def split_heads(self,tensor) : 
+        batch_size, seq_length, d_model = tensor.size()
+        tensor = tensor.view(batch_size, seq_length, self.num_heads, d_model // self.num_heads)
+        tensor = tensor.transpose(1,2)
+        return tensor
+            
+    def scaled_dot_product_attention(query, key, value, mask=None):
+        scores = torch.matmul(query, key.transpose(-2, -1)) / torch.sqrt(torch.tensor(query.size(-1), dtype=torch.float32))
+        
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, float("-1e20"))  # Masquage des scores
+        
+        attention_weights = torch.nn.functional.softmax(scores, dim=-1)
+        
+        output = torch.matmul(attention_weights, value)
+        
+        return output, attention_weights
+    
+    def forward(self, query, key, value):
+        q = self.query(query)
+        k = self.key(key)
+        v = self.value(value)
+        
+        q = self.split_heads(q)
+        k = self.split_heads(k)
+        v = self.split_heads(v)
+        
+        scores = torch.matmul(q, k.transpose(-2, -1)) / np.sqrt(self.d_model)
+        attention_weights = torch.nn.functional.softmax(scores, dim=-1)
+        attention_output = torch.matmul(attention_weights, v)
+        
+        attention_output = attention_output.transpose(1,2).contiguous()
+        attention_output = attention_output.view(attention_output.size(0), -1, self.d_model)
+        
+        output = self.final_projection(attention_output)
+        return output, attention_weights
+
+        
---
+++ transformer/PositionalEncoding.py
@@ -0,0 +1,34 @@
+"""
+Project : CamemBERT
+Unit : Advanced Machine Learning 
+MSc. Intelligent systems engineering
+SORBONNE UNIVERSITÉ
+
+--- Students ---
+@KishanthanKingston - Kishanthan KINGSTON
+@SSivanesan - Shivamshan SIVANESAN
+@Emirtas7 - Emir TAS
+"""
+
+# PositionalEncoding.py
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=512):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(0.1)
+        
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float32).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float32) * -(np.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)        
