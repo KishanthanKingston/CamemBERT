c5eef72d9076cbf8b3b5013a880be73cf19e23ee
Kishanthan Kingston <115884616+KishanthanKingston@users.noreply.github.com>
2023-11-22 06:29:03 +0100

    Add files via upload
    Segment and Postional Embedding, a first and a quicker version. Need to be adapt for RoBERTa Architecture.

---
+++ PositionalEmbedding.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def positional_embedding(max_len, d_model):
+    """
+    Crée une matrice d'embedding positionnel.
+
+    Entrée:
+    - max_len (int): La longueur maximale de la séquence.
+    - d_model (int): La dimension de l'embedding.
+
+    Sortie:
+    - numpy array
+    """
+    pos = np.arange(max_len).reshape(-1, 1)
+    div_term = np.exp(np.arange(0, d_model, 2) * -(np.log(10000.0) / d_model))
+    pos_embedding = np.zeros((max_len, d_model))
+
+    # Calcul de l'embedding positionnel
+    pos_embedding[:, 0::2] = np.sin(pos * div_term)
+    pos_embedding[:, 1::2] = np.cos(pos * div_term)
+
+    return pos_embedding
