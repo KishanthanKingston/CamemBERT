0b7634e980e953e04169106e70a150a02cdba965
Shivamshan Sivanesan <116385693+SShivamshan@users.noreply.github.com>
2023-12-17 13:41:21 +0100

    Update main.py
    Cleaned the main.py file

--- main.py
+++ main.py
@@ -21,36 +21,18 @@ from EvaluateRoBERTa import EvaluateRoBERTa
 from MLM_Roberta_POS_Tag import MLM_Roberta_POS_Tag
 # Main file
 
-
-# 6689783 input dans fr_part_1.txt
 if __name__ == "__main__":
-    pass
-    # pre_process = PreProcessing('fr_part_1.txt')
+    ## First training of MLM Roberta
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    roberta = MLM_RoBERTa(vocab_size=32000, ff_dim=768, hidden_size=768)
+    roberta.to(device)
 
-    # input = pre_process.read_dataset()
-    
-    # pre_process.sentence_piece()
-    # id = random.randint(0,100)
-    # # print(id)
-    # data = input[id]
-    # print(data)
-    
-    #masked_tokens,masked_label = pre_process.dynamic_masking(pre_process.sentence_token(data))
-    # _tokens = pre_process.sentence_token("On pourra toujours parler à propos d'Averroès de \"décentrement du Sujet\" " )
-    # print(_tokens)
-
-    # First training of MLM Roberta
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # # print('From main : ',device)
-    # roberta = MLM_RoBERTa(vocab_size=32000, ff_dim=768, hidden_size=768)
-    # roberta.to(device)
-
-    # criterion = nn.CrossEntropyLoss()
-    # roberta.train_mlm(criterion)
+    criterion = nn.CrossEntropyLoss()
+    roberta.train_mlm(criterion)
     # roberta.test_mlm(criterion)
-    
-    # ## Test to verify if the current model and the saved model are the same or not 
     # torch.save(roberta.state_dict(), "MLM_RoBERTa.pth")
+
+    ## Test to verify if the current model and the saved model are the same or not 
     # saved_state_dict = torch.load('MLM_RoBERTa.pth')
 
     # Print keys and sizes from the saved state dictionary
@@ -62,25 +44,6 @@ if __name__ == "__main__":
     # for name, param in roberta.named_parameters():
     #     print(f"Layer: {name}, Size: {param.size()}")
 
-    ##  Fine tunning MLM_RoBERTa model for POS-tag
-
-    # pos_tag = MLM_Roberta_POS_Tag('MLM_RoBERTa.pth',train_set='fr_gsd-ud-train.conllu',test_set='fr_gsd-ud-test.conllu')
-    # pos_tag.to(device)
-
-    # criterion = nn.CrossEntropyLoss()
-    # pos_tag.train_pos_tag(criterion)
-
-
-
-    ## Evaluate the MLM_roBERTa model for task specific POS-tag
-    # evaluate = EvaluateRoBERTa('MLM_RoBERTa.pth', 'm_model.model',ff_dim=768,vocab_size=32000)
-    # sentences = evaluate.read_conll_file('fr_gsd-ud-test.conllu')
-    # print(sentences[0]['text'])
-    # print(len(sentences[0]['labels']))
-
-
-
-    # evaluate.evaluate_pt(sentences[0]['text'],sentences[0]['labels'])
 
     
     
